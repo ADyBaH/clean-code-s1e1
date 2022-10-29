25fcdfea28c98116b1996490628a94421b594d42
ADyBaH <Brandon4@mail.ru>
2022-10-29 20:34:00 +0300

    refactor: add formatting JS according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ var createNewTaskElement=function(taskString){
 
     //Each elements, needs appending
     checkBox.type="checkbox";
+    checkBox.className="checkbox-in-li"
     editInput.type="text";
     editInput.className="text-area-task";
 
