f781bb699503f6aedaff004946562ac7e0c116f7
ADyBaH <Brandon4@mail.ru>
2022-10-29 20:07:26 +0300

    refactor: add formatting HTML and JS according to rule 3.3 in html-and-css.md

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ var createNewTaskElement=function(taskString){
 
     deleteButton.className="delete-button";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.className="delete-button-img"
     deleteButton.appendChild(deleteButtonImg);
 
 
